309649cc8bcf3606fab077dd328d9da6b8cdfaf1
Ali-Atif <126669357+Ali-Atif@users.noreply.github.com>
2024-12-28 01:20:33 +0500

    Time: 0 ms (100.00%) | Memory: 23.8 MB (42.98%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        for(int i=0;i<nums.size();i++){
-            for(int j=1;j<nums.size();j++){
-                if(nums[j-1] ==0){
-                swap(nums[j-1],nums[j]);
-                }
+        int nonZeroIndex = 0; // Pointer to place non-zero elements
+
+        // Step 1: Move all non-zero elements to the front
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                nums[nonZeroIndex] = nums[i];
+                nonZeroIndex++;
             }
-    }
+        }
+
+        // Step 2: Fill the remaining positions with zeros
+        for (int i = nonZeroIndex; i < nums.size(); i++) {
+            nums[i] = 0;
+        }
     }
 };
